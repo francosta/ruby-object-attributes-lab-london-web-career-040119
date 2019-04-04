ea73df62a5f297680f7f8ba3f5bbfbe7ab46b4cb
Francisco Costa <francisco@fcosta.pt>
2019-04-04 13:45:40 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -14,3 +14,19 @@ class Dog
   
   
 end
+
+class Person
+ 
+  def initialize(name)
+    @name = name
+  end
+ 
+  def name
+    @name
+  end
+ 
+  def name=(new_name)
+    @name = new_name
+  end
+ 
+end
